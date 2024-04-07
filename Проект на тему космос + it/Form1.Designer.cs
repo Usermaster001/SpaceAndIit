8c2b0489a1101d097f455fe54a91c3a70007a873
towor <towor@COMPUTER>
2024-04-07 19:00:03 +0300

    Добавьте файлы проекта.

---
+++ Проект на тему космос + it/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Проект_на_тему_космос___it
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox11.BackgroundImage = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_космос;
+            this.pictureBox11.Image = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_Нептун;
+            this.pictureBox11.Location = new System.Drawing.Point(20, 738);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 10;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.pictureBox11_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox10.BackgroundImage = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_космос;
+            this.pictureBox10.Image = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_всех_планет;
+            this.pictureBox10.Location = new System.Drawing.Point(131, 12);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(280, 280);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox9.BackgroundImage = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_космос;
+            this.pictureBox9.Image = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_Уран;
+            this.pictureBox9.Location = new System.Drawing.Point(12, 648);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(90, 60);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox8.BackgroundImage = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_космос;
+            this.pictureBox8.Image = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_меркурий;
+            this.pictureBox8.Location = new System.Drawing.Point(20, 105);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.BackgroundImage = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_космос;
+            this.pictureBox7.Image = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_венера;
+            this.pictureBox7.Location = new System.Drawing.Point(20, 198);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.BackgroundImage = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_космос;
+            this.pictureBox6.Image = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_земля;
+            this.pictureBox6.Location = new System.Drawing.Point(20, 290);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.BackgroundImage = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_космос;
+            this.pictureBox5.Image = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_марс;
+            this.pictureBox5.Location = new System.Drawing.Point(20, 377);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_космос;
+            this.pictureBox4.Image = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_Юпитер;
+            this.pictureBox4.Location = new System.Drawing.Point(20, 465);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_космос;
+            this.pictureBox3.Image = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_Сатурн;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 569);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(90, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_космос;
+            this.pictureBox2.Image = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_солнца;
+            this.pictureBox2.Location = new System.Drawing.Point(20, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(131, 350);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1190, 461);
+            this.panel1.TabIndex = 11;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1184, 455);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.richTextBox1.Location = new System.Drawing.Point(518, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 280);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Проект_на_тему_космос___it.Properties.Resources.Картинка_космос;
+            this.ClientSize = new System.Drawing.Size(1350, 823);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Planets";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Проект на тему космос + it/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Проект_на_тему_космос___it
+{
+    public partial class Form1 : Form
+    {
+        private AllDataAboutPlanets alldataplanets = new AllDataAboutPlanets();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            HeadAction(pictureBox10, alldataplanets.nameallplanets,
+                alldataplanets.urlallplanets, false);
+        }
+
+        private void GenerationVideo(string url)
+        {
+            var embed = "<html><head>" +
+                "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\"/>" +
+                "</head></body>" +
+                "<iframe width\"300\" src=\"{0}\"" +
+                "frameborder = \"0\" allow = \"autoplay; encryted-media\" allowfullscreen></iframe>" +
+                "</body></html>";
+
+            webBrowser1.DocumentText = string.Format(embed, url);
+        }
+
+        private void HeadAction(PictureBox pictureBox, string nameplanet,
+            string urlplanet, bool check)
+        {
+            if (check)
+            {
+                pictureBox10.ClientSize = new System.Drawing.Size(360, 200);
+            }
+            else
+            {
+                pictureBox10.ClientSize = new System.Drawing.Size(280, 280);
+            }
+            pictureBox10.Image = pictureBox.Image;
+            pictureBox10.Show();
+
+            richTextBox1.Text = nameplanet;
+            richTextBox1.Show();
+
+            GenerationVideo(urlplanet);
+        }
+        
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            HeadAction(pictureBox8, alldataplanets.namemercury,
+                alldataplanets.urlmercury, false);
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            HeadAction(pictureBox9, alldataplanets.nameuranus,
+                alldataplanets.urluranus, true);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            HeadAction(pictureBox2, alldataplanets.namesun,
+                alldataplanets.urlsun, false);
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            HeadAction(pictureBox7, alldataplanets.namevenus,
+                alldataplanets.urlvenus, false);
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            HeadAction(pictureBox6, alldataplanets.nameearth,
+                alldataplanets.urlearth, false);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            HeadAction(pictureBox5, alldataplanets.namemars,
+            alldataplanets.urlmars, false);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            HeadAction(pictureBox4, alldataplanets.namejupiter,
+                alldataplanets.urljupiter, false);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            HeadAction(pictureBox3, alldataplanets.namesaturn,
+                alldataplanets.urlsaturn, true);
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            HeadAction(pictureBox11, alldataplanets.nameneptune,
+                alldataplanets.urlneptune, false);
+        }
+    }
+}
